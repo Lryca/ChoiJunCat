6c97f0a9af2c238f873a7d7e7d2c7050c2f7fc23
Lryca <72592692+Lryca@users.noreply.github.com>
2021-08-09 01:49:30 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,12 @@ from discord.ext import commands
 import os
 import random
 
-client = commands.Bot(command_prefix = '-')
-
 class chatbot(discord.Client):
     # 프로그램이 처음 실행되었을 때 초기 구성
     async def on_ready(self):
         # 상태 메시지 설정
         # 종류는 3가지: Game, Streaming, CustomActivity
-        game = discord.Game("식사")
+        game = discord.Game("유혹")
 
         # 계정 상태를 변경한다.
         # 온라인 상태, game 중으로 설정
